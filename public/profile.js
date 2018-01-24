ff13aa178b8881804fa9ffff32280d7eb7cc7b55
שחר <shachar@shrs-MacBook-Air.local>
2018-01-24 15:03:39 +0200

    updated mentor profile

---
+++ public/profile.js
@@ -0,0 +1,58 @@
+// get request for createing a route for profile page
+
+// var createProfileRoute = function () {
+//     $.ajax({
+//         method: "GET",
+//         url: '/userId',
+//         dataType: "json",
+//         success: function (data) {
+//             console.log('data returned!');
+//         },
+//         error: function (jqXHR, textStatus, errorThrown) {
+//             console.log(textStatus);
+//           }
+//         });
+//     };
+
+var mentors =[]
+
+//get the relevent mentor data
+var fetch = function() {
+    $.ajax({
+        method:"GET",
+        url:'mentor-profile',
+        success: function(data) {
+            console.log(data);
+            mentors = data;
+            renderMentorProfile();
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+                     console.log(textStatus);
+                 }
+    });
+};
+
+// render the mentor page with relevant data
+function renderMentorProfile() {
+    var source = $('#profile-template').html();
+    var template = Handlebars.compile(source);
+    var newHTML = template (mentors);
+     $('.profile-content').append(newHTML);
+}
+
+
+//   var fetch = function () {
+//     $.ajax({
+//       method: "GET",
+//       url: '/posts',
+//       success: function (data) {
+//         console.log(data);
+//         posts = data;
+//         _renderPosts();
+
+//       },
+//       error: function (jqXHR, textStatus, errorThrown) {
+//         console.log(textStatus);
+//       }
+//     });
+//   };
