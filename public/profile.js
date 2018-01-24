cac2f9e64792ec0c43ca0acadfe1c3ea73b0da6c
שחר <shachar@shrs-MacBook-Air.local>
2018-01-24 21:05:28 +0200

    form-data

--- public/profile.js
+++ public/profile.js
@@ -45,18 +45,38 @@ function renderMentorProfile() {
 }
 
 
-//   var fetch = function () {
-//     $.ajax({
-//       method: "GET",
-//       url: '/posts',
-//       success: function (data) {
-//         console.log(data);
-//         posts = data;
-//         _renderPosts();
+// on click 'send inquiry' btn 
 
-//       },
-//       error: function (jqXHR, textStatus, errorThrown) {
-//         console.log(textStatus);
+$('.profile-content').on('click','.send-form', function() {
+        var message = $(this).siblings("textarea").val();
+        var chosenTime = $(this).siblings("select").find("option").filter(":selected").text();
+        alert(chosenTime);
+        var bookingRequest = {time: chosenTime, message: message};
+        console.log(bookingRequest);
+        
+});
+
+
+
+
+
+// $posts.on('click', '.add-comment', function () {
+    
+//       var $comment = $(this).siblings('.comment');
+//       var $user = $(this).siblings('.name');
+    
+//       if ($comment.val() === "" || $user.val() === "") {
+//         alert("Please enter your name and a comment!");
+//         return;
 //       }
+    
+//       var postIndex = $(this).closest('.post').index();
+//       var newComment = { text: $comment.val(), user: $user.val() };
+    
+//       app.addComment(newComment, postIndex);
+    
+//       $comment.val("");
+//       $user.val("");
+    
 //     });
-//   };
+    
